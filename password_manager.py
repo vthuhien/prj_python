5fec437b9d9ba7cb00b57d03ba6cca62f42cafb0
banhiuoi𐙚 <142284485+vthuhien@users.noreply.github.com>
2025-02-20 04:30:45 +0000

    o

--- password_manager.py
+++ password_manager.py
@@ -1,8 +1,20 @@
+from cryptography.fernet import Fernet #this model allows you to encrypt text 
+
 a = input("What is the master password? ")
+# if you enter the wrong pass, the decrypted text will be incorrect
 
+# make key for us
+def main_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb"): #use wb for binary file
+        pass
 def view ():
-    pass
-
+    with open('password.txt', 'r') as read_f:
+        for line in read_f.readlines():
+            data = line.rstrip()
+            acc, passw = data.split("|")
+            print("Account :", acc)
+            print("Password: ", passw, "\n")
 
 
 def add():
@@ -12,7 +24,8 @@ def add():
     # file = open('ps.txt','a')
     # file.close()
     with open('password.txt', 'a') as f:
-        f.write(acc + "|" + passw)
+        f.write(acc + "|" + passw + "\n")
+        #pip install cryptography for password
 
 
 while True:
